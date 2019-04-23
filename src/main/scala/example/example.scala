6c1a1225de74950261a596a03c41f4988f9a9d74
Aaron <aaron.pritzlaff@gmail.com>
2019-04-23 08:39:21 +0100

    docs from example

--- src/main/scala/example/example.scala
+++ src/main/scala/example/example.scala
@@ -1,13 +1,20 @@
 package example
 
-import akka.http.scaladsl.server.Route
 import endpoints.openapi.model.{Info, OpenApi}
 import endpoints.{algebra, openapi}
 import io.circe.generic.semiauto._
-import io.circe.{Decoder, ObjectEncoder}
+import io.circe._
 
 
-case class SomeRequest(data: String)
+case class Reference(key: String, value: Long)
+
+object Reference {
+  implicit def encoder: ObjectEncoder[Reference] = deriveEncoder[Reference]
+
+  implicit def decoder: Decoder[Reference] = deriveDecoder[Reference]
+}
+
+case class SomeRequest(data: String, int: Int, flag: Option[Boolean], ref: Reference)
 
 object SomeRequest {
   implicit def encoder: ObjectEncoder[SomeRequest] = deriveEncoder[SomeRequest]
@@ -18,12 +25,7 @@ object SomeRequest {
 
 case class SomeResponse(data: Int)
 
-trait SomeEndpoint extends algebra.Endpoints with endpoints.algebra.JsonSchemaEntities with endpoints.generic.JsonSchemas  { //with endpoints.algebra.JsonSchemas
-
-  //  implicit def requestSchema: JsonSchema[SomeRequest] //= lazySchema(implicitly, implicitly)
-  //  implicit def responseSchema: JsonSchema[SomeResponse] // = lazySchema(implicitly, implicitly)
-
-  implicit lazy val someRequestSchema: Record[SomeRequest] = field[String]("data").xmap(SomeRequest.apply)(_.data)
+trait SomeEndpoint extends algebra.Endpoints with endpoints.algebra.JsonSchemaEntities {
 
   def someDocumentedResource(implicit req: JsonRequest[SomeRequest]): Endpoint[(Int, SomeRequest), String] = {
     val request = post(path / "some-resource" / segment[Int]("id"), jsonRequest[SomeRequest](Option("perform an action")))
@@ -33,52 +35,19 @@ trait SomeEndpoint extends algebra.Endpoints with endpoints.algebra.JsonSchemaEn
     )
   }
 }
-//
-//object AkkaServer extends endpoints.circe.JsonSchemas with endpoints.akkahttp.server.JsonSchemaEntities with SomeEndpoint {
-//  implicit def requestSchema: JsonSchema[SomeRequest] = JsonSchema(implicitly, implicitly)
-//
-//  //  implicit def someReq = jsonRequest[SomeRequest](Option("meh"))
-//
-//  val route: Route = someDocumentedResource.implementedBy {
-//    case (a, b) => s"got $a w/ $b"
-//  }
-//}
 
 
 object OpenApiEncoder extends endpoints.openapi.model.OpenApiSchemas with endpoints.circe.JsonSchemas {
   implicit def requestSchema: JsonSchema[SomeRequest] = JsonSchema(implicitly, implicitly)
-
 }
 
 
-//
-//  this fails to compile
-// vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-object CirceDocs extends openapi.Endpoints with SomeEndpoint with openapi.JsonSchemaEntities {
-
-  import OpenApiEncoder._
-
-  //  val foo = jsonRequest[SomeRequest](Option("perform an action"))
-
+object CirceDocs extends openapi.Endpoints with SomeEndpoint with openapi.JsonSchemaEntities with endpoints.generic.JsonSchemas with CirceAdapter {
 
   val api: OpenApi = {
-    //    val x: _root_.example.CirceDocs.DocumentedJsonSchema = implicitly[JsonRequest[SomeRequest]]
-
-    implicit val x: OpenApiEncoder.JsonSchema[SomeRequest] = OpenApiEncoder.JsonSchema.apply[SomeRequest](implicitly, implicitly)
-
-    implicit def requestSchema: OpenApiEncoder.JsonSchema[SomeRequest] = OpenApiEncoder.JsonSchema[SomeRequest](SomeRequest.encoder, SomeRequest.decoder) //(implicitly, implicitly)
-
-//    implicit val weSomehowNeedThis: JsonRequest[SomeRequest] = {
-    ////      // have to produce a
-    ////      val someReq  = jsonRequest[SomeRequest](Option("foo")) //(requestSchema)
-    ////      ???
-    ////    }
-    //    implicit val sc: DocumentedJsonSchema = null // ??? //lazySchema(x)
-    //    val r = jsonRequest[SomeRequest](None)
-    //    val r : JsonRequest[SomeRequest] = jsonRequest[SomeRequest](None)
-    openApi(Info(title = "API to get some resource", version = "1.0"))(
-      someDocumentedResource
-    )
+    openApi(Info(title = "API to get some resource", version = "1.0")) {
+      someDocumentedResource(document(SomeRequest("hello", 1, Option(true), Reference("key", 123))))
+    }
   }
 
   import OpenApiEncoder.JsonSchema._
@@ -92,7 +61,5 @@ object CirceDocs extends openapi.Endpoints with SomeEndpoint with openapi.JsonSc
 object Main extends App {
   println("open api docs:")
   println(CirceDocs.apiJson.spaces4)
-  println("akka route :")
-//  println(AkkaServer.route)
 
 }
